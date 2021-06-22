a7cc38ab24b431c827c68c8786effce87669ccf0
eric <lee.eric.03@gmail.com>
2021-06-22 05:58:20 -0600

    ensure backup

--- backend/app/controllers/movies_controller.rb
+++ backend/app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+
+    render json: @movies
+  end 
+
+  def destroy
+    @movie = Movie.find params[:id] 
+    @movie.destroy
+  end 
+
+  def new
+    @movie = Movie.new 
+
+    render json: @movie 
+  end 
+
+  def create
+    @movie = Movie.create params[:title], params[:director], params[:release_year], params[:poster_url]
+  end 
+  
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :movies, through: :reviews
 
+    validates :username, uniqueness: true
+
     def movie_names
       movies.pluck(:name)
     end 
